01b6c17ef23542834713eb18ab98b28a25bb11c4
Bryan See <junzhisee@Juns-MacBook-Pro.local>
2020-12-22 23:36:37 -0800

    Able to grab fixtures

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import re
 import sys
 import requests
-import pandas as pd
+import pandas as pd 
+import pytz
+from datetime import date, datetime
 from bs4 import BeautifulSoup
 
 def create_standings():
@@ -29,6 +31,39 @@ def create_standings():
     standings['Goal Difference'] = standings['Goals For'].astype(int) - standings['Goals Against'].astype(int)
     return standings
 
+def convert_time(time):
+    pytz_time = datetime.strptime(uk_time, '%Y-%m-%d %H:%M:%S')
+    tz_ldn = pytz.timezone('Europe/London')
+    tz_la = pytz.timezone('America/Los_Angeles')
+    ldn_time = tz_ldn.localize(pytz_time)
+    la_time = ldn_time.astimezone(tz_la)
+    return la_time.time()
+
+today = re.sub(r'-', '', str(date.today()))
+page = requests.get('https://www.espn.com/soccer/fixtures/_/date/' + today + '/league/eng.1')
+soup = BeautifulSoup(page.content, 'html.parser')
+count = 0
+while True:
+    try:
+        fixtures = soup.find_all('table')[count].find_all('tr')
+        date_caption = soup.find_all('h2', class_= 'table-caption')[count]
+        date = re.search(r'>([^+]*)<', str(date_caption)).group(1)
+    except:
+        break
+    print(date)
+    print("--------------------------------------------")
+    for element in fixtures[1:]:
+        spans = element.find_all('span')
+        team1 = re.search(r'>([^+]*)<' ,str(spans[0]))
+        team2 = re.search(r'>([^+]*)<' ,str(spans[-1]))
+        time = re.search(r'([\d]+-[\d]+-[\d]+)T([\d]+:[\d]+)Z', str(element))
+        uk_time = time.group(1) + " " + time.group(2) + ":00"
+        cur_time = str(convert_time(uk_time))
+        if team1 and team2:
+            print(team1.group(1) + ' vs ' + team2.group(1) + ' at ' + cur_time + ' Los Angeles Time')
+    print("--------------------------------------------")
+    count += 1
+    
 def main():
     print("Welcome to Live EPL")
     print("--------------------------------------------")
@@ -51,7 +86,6 @@ def main():
             print("Please type a valid command.")
             print("Type in 'help' for list of commands.")
         print("--------------------------------------------")
-
 
 standings_table = create_standings()
 main()
