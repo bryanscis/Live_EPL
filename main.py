ef25dcb56756f7ce189ec8e301896f8eb231367e
Bryan See <junzhisee@Juns-MacBook-Pro.local>
2020-12-22 23:58:26 -0800

    Formatted code to look neater

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import pytz
 from datetime import date, datetime
 from bs4 import BeautifulSoup
 
+def team_string_check(name):
+    return re.sub(r'&amp;', '&', name)
+
 def create_standings():
     page = requests.get("https://www.espn.com/soccer/table/_/league/eng.1")
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -15,8 +18,7 @@ def create_standings():
     for team in table1:
         team_name = re.search(r'img alt="([^"]*)', str(team))
         if team_name:
-            team_name = team_name.group(1)
-            team_name = re.sub(r'&amp;', '&', str(team_name))
+            team_name = team_string_check(str(team_name.group(1)))
             teams.append(team_name)
     table2 = soup.find_all('table')[1].find_all('tr')
     statistics= {}
@@ -32,37 +34,40 @@ def create_standings():
     return standings
 
 def convert_time(time):
-    pytz_time = datetime.strptime(uk_time, '%Y-%m-%d %H:%M:%S')
+    pytz_time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
     tz_ldn = pytz.timezone('Europe/London')
     tz_la = pytz.timezone('America/Los_Angeles')
     ldn_time = tz_ldn.localize(pytz_time)
     la_time = ldn_time.astimezone(tz_la)
     return la_time.time()
 
-today = re.sub(r'-', '', str(date.today()))
-page = requests.get('https://www.espn.com/soccer/fixtures/_/date/' + today + '/league/eng.1')
-soup = BeautifulSoup(page.content, 'html.parser')
-count = 0
-while True:
-    try:
-        fixtures = soup.find_all('table')[count].find_all('tr')
-        date_caption = soup.find_all('h2', class_= 'table-caption')[count]
-        date = re.search(r'>([^+]*)<', str(date_caption)).group(1)
-    except:
-        break
-    print(date)
-    print("--------------------------------------------")
-    for element in fixtures[1:]:
-        spans = element.find_all('span')
-        team1 = re.search(r'>([^+]*)<' ,str(spans[0]))
-        team2 = re.search(r'>([^+]*)<' ,str(spans[-1]))
-        time = re.search(r'([\d]+-[\d]+-[\d]+)T([\d]+:[\d]+)Z', str(element))
-        uk_time = time.group(1) + " " + time.group(2) + ":00"
-        cur_time = str(convert_time(uk_time))
-        if team1 and team2:
-            print(team1.group(1) + ' vs ' + team2.group(1) + ' at ' + cur_time + ' Los Angeles Time')
-    print("--------------------------------------------")
-    count += 1
+def display_upcoming_fixtures():
+    today = re.sub(r'-', '', str(datetime.today()))
+    page = requests.get('https://www.espn.com/soccer/fixtures/_/date/' + today + '/league/eng.1')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    count = 0
+    while True:
+        try:
+            fixtures = soup.find_all('table')[count].find_all('tr')
+            date_caption = soup.find_all('h2', class_= 'table-caption')[count]
+            date = re.search(r'>([^+]*)<', str(date_caption)).group(1)
+        except:
+            break
+        print("--------------------------------------------")
+        print(date)
+        print("--------------------------------------------")
+        for element in fixtures[1:]:
+            spans = element.find_all('span')
+            team1 = re.search(r'>([^+]*)<' ,str(spans[0]))
+            team2 = re.search(r'>([^+]*)<' ,str(spans[-1]))
+            time = re.search(r'([\d]+-[\d]+-[\d]+)T([\d]+:[\d]+)Z', str(element))
+            uk_time = time.group(1) + " " + time.group(2) + ":00"
+            cur_time = str(convert_time(uk_time))
+            if team1 and team2:
+                print(team_string_check(str(team1.group(1)))
+                      + ' vs ' + team_string_check(str(team2.group(1))) 
+                      + ' at ' + cur_time + ' Los Angeles Time')
+        count += 1
     
 def main():
     print("Welcome to Live EPL")
@@ -78,8 +83,10 @@ def main():
             break
         if command == 'standings':
             print(standings_table)
+        elif command == 'fixtures':
+            display_upcoming_fixtures()
         elif command == 'exit':
-            print("\nSuccessfully Exited")
+            print("Successfully Exited")
             break
         else:
             print(command + " is not a valid command.")
