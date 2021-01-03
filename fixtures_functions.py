5a1af6683f925286f2eab0e1254ade10c059edd1
Bryan See <bryanscis@gmail.com>
2021-01-02 19:00:57 -0800

    Fixed display_fixtures() to work with postponed matches

--- fixtures_functions.py
+++ fixtures_functions.py
@@ -21,38 +21,41 @@ def print_divider():
 def display_fixtures():
     page = requests.get('https://www.espn.com/soccer/fixtures/_/league/eng.1')
     soup = BeautifulSoup(page.content, 'html.parser')
-    count = 0
-    while True:
-        try:
-            fixtures = soup.find_all('table')[count].find_all('tr')
-            date_caption = soup.find_all('h2', class_= 'table-caption')
-            if count != 0:
-                print_divider()
-        except: 
-            break
-        print(date_caption[count].get_text())
-        print_divider()
-        for element in fixtures[1:]:
-            spans = element.find_all('span')
-            team1 = re.search(r'>([^+]*)<' ,str(spans[0]))
-            team2 = re.search(r'>([^+]*)<' ,str(spans[-1]))
-            if element.find('td', class_='live'):
-                if team1 and team2:
-                    print(team_string_check(str(team1.group(1)))
-                        + ' vs ' + team_string_check(str(team2.group(1))) 
-                        + ' is LIVE right now in Los Angeles Time')
-            elif element.find( attrs={'name': ('&lpos=eng.1:schedule:score')}):
-                final_score = element.find('span', class_='record').get_text()
-                if team1 and team2:
-                    print(team_string_check(str(team1.group(1)))
-                        + ' vs ' + team_string_check(str(team2.group(1))) 
-                        + ' ended in ' + final_score)
-            else:
-                time = re.search(r'([\d]+-[\d]+-[\d]+)T([\d]+:[\d]+)Z', str(element))
-                uk_time = time.group(1) + " " + time.group(2) + ":00"
-                cur_time = str(convert_time(uk_time))
-                if team1 and team2:
-                    print(team_string_check(str(team1.group(1)))
-                        + ' vs ' + team_string_check(str(team2.group(1))) 
-                        + ' at ' + cur_time + ' Los Angeles Time')
-        count += 1
+    for tag in soup.find('div',id='sched-container').children:
+        if tag.find('table'):
+            fixtures = tag.find_all('tr')
+            for element in fixtures[1:]:
+                spans = element.find_all('span')
+                team1 = re.search(r'>([^+]*)<' ,str(spans[0]))
+                team2 = re.search(r'>([^+]*)<' ,str(spans[-1]))
+                match_status = element.find(attrs={'name': ('&lpos=eng.1:schedule:score')})
+                if element.find('td', class_='live'):
+                    if team1 and team2:
+                        print(team_string_check(str(team1.group(1)))
+                            + ' vs ' + team_string_check(str(team2.group(1))) 
+                            + ' is LIVE right now in Los Angeles Time')
+                elif match_status:
+                    final_score = element.find('span', class_='record').get_text()
+                    if match_status.get_text() == 'Postponed':
+                        if team1 and team2:
+                            print(team_string_check(str(team1.group(1)))
+                                  + ' vs ' + team_string_check(str(team2.group(1)))
+                                  + ' has been postponed to a later date')
+                    elif match_status.get_text() == 'FT':
+                        if team1 and team2:
+                            print(team_string_check(str(team1.group(1)))
+                                + ' vs ' + team_string_check(str(team2.group(1))) 
+                                + ' ended in ' + final_score)
+                else:
+                    time = re.search(r'([\d]+-[\d]+-[\d]+)T([\d]+:[\d]+)Z', str(element))
+                    uk_time = time.group(1) + " " + time.group(2) + ":00"
+                    cur_time = str(convert_time(uk_time))
+                    if team1 and team2:
+                        print(team_string_check(str(team1.group(1)))
+                            + ' vs ' + team_string_check(str(team2.group(1))) 
+                            + ' at ' + cur_time + ' Los Angeles Time')
+        elif tag.name == 'h2':
+            print('')
+            print(tag.get_text())
+            print_divider()
+            
